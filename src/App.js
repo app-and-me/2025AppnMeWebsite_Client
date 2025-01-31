6e2a2b88358c21f5a17adede094bd1bbca7982d9
juyeon Bae <w2316@e-mirim.hs.kr>
2025-02-01 03:36:08 +0900

    feat : React Router를 사용해 페이지 라우팅 설정 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-
+import HomePage from "./components/Common/Header";
+import AboutUsPage from "./pages/AboutUsPage";
+import react from "../src/styles/Style.module.css"
 function App() {
   return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
+    <Router>
+      <Main />
+    </Router>
   );
+};
+
+const Main = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/Home" element={<HomePage />} />
+        <Route path="/AboutUs" element={<AboutUsPage />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
