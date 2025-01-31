2721a1d2ce9ae40ef308814ed9cd1eb318b8e48e
juyeon Bae <w2316@e-mirim.hs.kr>
2025-02-01 00:56:22 +0900

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>test</h1>
+    </div>
+  );
+}
+
+export default App;
