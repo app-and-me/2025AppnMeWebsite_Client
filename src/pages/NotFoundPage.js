b18f4f2d4372954b1589ea0491ac4dac3dbac415
juyeon Bae <w2316@e-mirim.hs.kr>
2025-02-03 19:43:16 +0900

    feat : PageNotFount page 컴포넌트 구현

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Common/Button";
+import styles from "../styles/NotFoundPage.module.css"
+
+export default function NotFound() {
+    const navigate = useNavigate();
+    const handleclick = () => {
+        navigate("/Home")
+    }
+    return (
+
+        <div className={styles["all-container"]}>
+            <div style={{ display: "flex", alignItems: "center", columnGap: "12px", margin: "20px 0 34px 0" }}>
+                <div className={styles["btn-container"]}>OOPS</div>
+                <span style={{ fontSize: "16px" }}>Page Not Found</span>
+            </div>
+
+            <div className={styles["main-title"]}>
+                <p>404 - Oops Page</p>
+                <p>Not Found in Data.</p>
+            </div>
+
+            <div className={styles["sub-title"]}>
+                <p>Oops! The web page you’re looking for doesn’t</p>
+                <p>exist. Please click the below button to get back to the home.</p>
+            </div>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <Button width={"170px"} height={"46px"} text={"Back To Homepage"} onClick={handleclick} /></div>
+        </div>
+    )
+}
