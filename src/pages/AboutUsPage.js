ab7e6b43ee40af1d98528641079218be3db37f67
juyeon Bae <w2316@e-mirim.hs.kr>
2025-02-04 12:43:22 +0900

    fix : border radius 추가

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import TitleCard from "../components/Common/TitleCard";
+import FaQTitle from "../components/QnA/FaQTitle";
 import JoinUsNow from "../components/Common/JoinUsNow";
-
+// import ProfileCard from "../components/AboutUs/ProfileCard";
 export default function AboutUsPage() {
     return (
         <div>
             <TitleCard
+                borderRadius={"6px"}
                 fontSize={"12px"}
                 btnWeight={"46px"}
                 btnHeight={"28px"}
@@ -14,7 +16,8 @@ export default function AboutUsPage() {
                 subTitle1={"12기, 13기의 앱앤미 부원들을 소개합니다!"}
                 subTitle2={"다음의 앱앤미가 될 14기 부원들을 기다리고 있답니다."}
             />
-
+            {/* <ProfileCard /> */}
+            <FaQTitle />
             <JoinUsNow />
         </div>
     )
