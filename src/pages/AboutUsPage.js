76e696b38b30688120e1f8dd8d9def46e449b07c
juyeon Bae <w2316@e-mirim.hs.kr>
2025-02-06 04:00:23 +0900

    feat : Add Banner Component

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,21 +1,28 @@
 import React from "react";
 import TitleCard from "../components/Common/TitleCard";
+import Banner from "../components/Home/MainTitle/Banner";
 import FaQTitle from "../components/QnA/FaQTitle";
 import JoinUsNow from "../components/Common/JoinUsNow";
 // import ProfileCard from "../components/AboutUs/ProfileCard";
 export default function AboutUsPage() {
     return (
         <div>
+            <div style={{position:"relative", display:"flex", height:"506px", overflow:"hidden"}}>
+            <Banner/>
+
+            <div style={{ position: "absolute", zIndex: "1", left: "50%", top: "50%", transform: "translate(-50%, -50%)" }}>
             <TitleCard
-                borderRadius={"6px"}
-                fontSize={"12px"}
-                btnWeight={"46px"}
-                btnHeight={"28px"}
-                btnText={"2025"}
+                borderRadius={"6px"}fontSize={"12px"}
+                btnWeight={"46px"} btnHeight={"28px"} btnText={"2025"}
                 mainTitle={"12기, 13기 부원을 소개합니다!"}
                 subTitle1={"12기, 13기의 앱앤미 부원들을 소개합니다!"}
                 subTitle2={"다음의 앱앤미가 될 14기 부원들을 기다리고 있답니다."}
             />
+            </div>
+            
+            </div>
+
+
             {/* <ProfileCard /> */}
             <FaQTitle />
             <JoinUsNow />
--- src/pages/PrizePage.js
+++ src/pages/PrizePage.js
@@ -1,10 +1,15 @@
 import TitleCard from "../components/Common/TitleCard";
+import Banner from "../components/Home/MainTitle/Banner";
 import FaQTitle from "../components/QnA/FaQTitle";
 import JoinUsNow from "../components/Common/JoinUsNow";
 
 export default function PrizePage() {
     return (
         <div>
+            <div style={{position:"relative", display:"flex", height:"506px", overflow:"hidden"}}>
+                <Banner/>
+                
+                <div style={{ position: "absolute", zIndex: "1", left: "50%", top: "50%", transform: "translate(-50%, -50%)" }}>
             <TitleCard
                 btnText={"NEW"}
                 btnWeight={"43px"}
@@ -14,6 +19,9 @@ export default function PrizePage() {
                 subTitle1={"멋진 상들을 받고 싶다면, 지금 지원하세요!"}
                 subTitle2={"멋진 선배들이 여러분을 간절히 기다리고 있습니다."}
             />
+            </div>
+        </div>
+
             <FaQTitle />
             <JoinUsNow />
         </div>
