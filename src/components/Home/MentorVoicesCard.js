8c44b87a99e7ae78856c21f019256e7ee11478c7
juyeon Bae <w2316@e-mirim.hs.kr>
2025-02-07 23:05:01 +0900

    fix : 12에게 앱앤미란? 내용 가져오기

--- src/components/Home/MentorVoicesCard.js
+++ src/components/Home/MentorVoicesCard.js
@@ -1,20 +1,41 @@
-import React from "react";
-import styles from "../../styles/Home/MentorVoices.module.css"
+import React, { useState, useEffect } from "react";
+import styles from "../../styles/Home/MentorVoices.module.css";
 
-export default function MentorVoicesCard({ profileImg, message, name, role, major }) {
+export default function MentorVoicesCard() {
+    const [mentorData, setMentorData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/data/mentorData.json");
+                const data = await response.json();  
+                setMentorData(data); // 상태에 데이터 저장
+            } catch (error) {
+                console.error("에러 발생", error);
+            }
+        }; 
+        fetchData();
+    }, []);  
+
     return (
-        <div className={styles["all-container"]}>
-            <img src={profileImg}></img>
-            <div className={styles["message"]}>{message}</div>
+        <div className={styles.card}>
+        <div className={styles.gridContainer}>
+            {mentorData.map((mentor) => (
+                <div key={mentor.id} className={styles["all-container"]}>
+                    <img src={`images/Member/member${mentor.id}.png`}/>
+                    <div className={styles["message"]}>{mentor.message}</div>
 
-            <div className={styles["memberInfo-container"]}>
-                <div className={styles.dotContainer}>
-                    <span>{name}</span>
-                    <div className={styles.dot}></div>
-                    <span>{role}</span>
+                    <div className={styles["memberInfo-container"]}>
+                        <div className={styles.dotContainer}>
+                            <span>{mentor.name}</span>
+                            <div className={styles.dot}></div>
+                            <span>{mentor.role}</span>
+                        </div>
+                        <span className={styles.major}>앱앤미 12기 - {mentor.major}</span>
+                    </div>
                 </div>
-                <span className={styles.major}>앱앤미 12기 - {major}</span>
-            </div>
+            ))}
         </div>
-    )
+        </div>
+    );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,10 +8,11 @@ import FaQTitle from "../components/QnA/FaQTitle";
 import MainActivity from "../components/Home/Activity/MainActivity";
 import MainAboutAppNMe from "../components/Home/AboutAppNMe/MainAboutAppNMe";
 import PrizeCard from "../components/Home/OurPrize/PrizeCard";
- 
+import styles from "../styles/Home/Pages/HomePage.module.css"
+
 export default function HomePage() {
     return (
-        <div>
+        <div className={styles.allContainer}>
             <MainTitle />
             <SectionBlock dotTitle={"APP & ME"} title1={"지금부터"} title2={"APP & ME를 소개합니다!"} marginBottom={"60px"}/>
             <MainAboutAppNMe />
@@ -33,13 +34,7 @@ export default function HomePage() {
             <SectionBlock dotTitle={"Testimonial"} title1={"12기 선배들에게"} title2={"앱앤미란?"} 
              marginTop={"75px"} marginBottom={"55px"}/>
 
-            <MentorVoicesCard
-                profileImg={"/images/Member/member1.png"}
-                message={"미림에 입학하고 앱앤미를 통해 정말 많은 경험을 통해 성장할 수 있었습니다. 특히 전공에서 앱앤미가 아니었다면 할 수 없는 성장을 한 것 같습니다. 다양한 외부활동과 협업 프로젝트는 큰 도움이 됐습니다. 또한 선배님들과도 다양한 소통과 스터디를 통해 많은 것들을 배울 수 있었습니다. 저에게 앱앤미는 후회 없는 선택이었습니다.로젝트는 큰 도움이 됐습니다. 또한 선배님들과도 다양한 소통과 스터디를 통해 많은 것들을 배울 수 있었습니다. 저에게 앱앤미는 후회 없는 선택이었습니다.로젝트는 큰 도움이 됐습니다. 또한 선배님들과도 다양한 소통과 스터디를 통해 많은 것들을 배울 수 있었습니다. 저에게 앱앤미는 후회 없는 선택이었습니다."}
-                role={"동아리짱"}
-                major={"소프트웨어과"}
-                name={"추다율"}
-            />
+            <MentorVoicesCard />
             <FaQTitle />
             {/* <FaQList /> */}
             <JoinUsNow />
