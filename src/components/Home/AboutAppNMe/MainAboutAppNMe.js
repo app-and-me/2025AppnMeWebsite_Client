b502e468486cbaf40033dff067b18ee2a4f6f79b
juyeon Bae <w2316@e-mirim.hs.kr>
2025-02-15 01:56:55 +0900

    fix : 반응형 수정

--- src/components/Home/AboutAppNMe/MainAboutAppNMe.js
+++ src/components/Home/AboutAppNMe/MainAboutAppNMe.js
@@ -14,11 +14,12 @@ export default function MainAboutAppNMe() {
             <img src="images/Logo/subLogo.png"></img>
 
             <div className={styles["title-container"]}>
-                <DotTitle dotTitle={"About App & Me"} />
+                <div className={styles.dotTitle}> <DotTitle dotTitle={"About App & Me"} /></div>
+
                 <p className={styles.mirim}>미림마이스터고등학교</p>
                 <Title title1={"자율 동아리 App & Me"} title2={"새로운 부원을 모집합니다!"} />
                 <div className={styles.content}><Content title1={"저희의 소개가 필요하다면 스크롤 해 주세요"} /></div>
-                <Button width={"89px"} height={"46px"} borderRadius={"10px"} text={"지원하기"} onClick={handleClick} />
+                <div className={styles.btn}><Button width={"89px"} height={"46px"} borderRadius={"10px"} text={"지원하기"} onClick={handleClick} /></div>
             </div>
         </div>
     )
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,6 @@ import { useNavigate } from "react-router-dom";
 import Project from "../components/Home/AboutAppNMe/Project";
 import Footer from "../components/Common/Footer";
 import LanguageCard from "../components/Home/LanguageCard";
-
 import ScrollingImages from "../components/Home/MainTitle/FlowCard";
 import MainTitle from "../components/Home/MainTitle/MainTitle";
 import SectionBlock from "../components/Home/SectionBlock";
@@ -19,46 +18,57 @@ import styles from "../styles/Home/Pages/HomePage.module.css"
 export default function HomePage() {
     return (
         <div className={styles.allContainer}>
-            <MainTitle />
-            <ScrollingImages />
-            <SectionBlock dotTitle={"APP & ME"} title1={"지금부터"} title2={"APP & ME를 소개합니다!"} marginBottom={"60px"} />
+
+            <div className={styles.mainWrapper}>
+                <MainTitle />
+                <ScrollingImages />
+            </div>
+
+            <div className={styles.introduce}><SectionBlock dotTitle={"APP & ME"} title1={"지금부터"}
+                title2={"APP & ME를 소개합니다!"} marginBottom={"60px"} /></div>
             <Bottom />
-            <MainAboutAppNMe />
+
+            <div style={{ margin: "100px 0" }}><MainAboutAppNMe /></div>
             <SectionBlock dotTitle={"Project"} title1={"App & Me"} title2={"프로젝트를 소개합니다!"}
                 subTitle1={"12기, 13기 앱앤미 부원들의 프로젝트들을 소개합니다!"} marginTop={"30px"} marginBottom={"30px"} />
-            <Project />
+            <div className={styles.styleContainer}><Project /></div>
 
-            <Bottom />
+            <div style={{ marginBottom: "100px" }}><Bottom /></div>
             <SectionBlock dotTitle={"Tools"} title1={"우리가 사용 / 배우는 툴"} subTitle1={"선배와 후배가 함께하는 스터디를 꾸준히 진행하여 부원들의"}
                 subTitle2={"실력 향상에 도움이 되고자 합니다. "} marginTop={"10px"} marginBottom={"40px"} />
+            <div className={styles.styleContainer}><LanguageCard /></div>
 
 
 
+            <div className={styles.styleContainer}>
+                <MainActivity />
+                <ActivityCardMain />
+            </div>
 
-            <LanguageCard />
-
-
-
-
-            <MainActivity />
-            <ActivityCardMain />
+            <div className={styles.styleContainer}>
+                <OurPrize />
+            </div>
 
             <SectionBlock dotTitle={"Photo"} title1={"앱앤미의 친목 사진"}
                 subTitle1={"선후배 간의 격식없이 다같이 친한 동아리로, 다양한 활동을 통해"}
                 subTitle2={"선배들과 이야기를 하며 친목을 다져 더욱 친밀하고 가까운 사이입니다."}
                 marginTop={"10px"} marginBottom={"65px"} />
-            <div className={styles.PhotoContainer}>
-                <Photo start={"0%"} end={"-100%"} images={[1, 2, 3, 4]} />
-                <Photo start={"-100%"} end={"0%"} images={[5, 6, 7, 8]} />
+            <div className={styles.styleContainer}>
+                <div className={styles.PhotoContainer}>
+                    <Photo start={"0%"} end={"-100%"} images={[1, 2, 3, 4]} />
+                    <Photo start={"-100%"} end={"0%"} images={[5, 6, 7, 8]} />
+                </div>
             </div>
 
-            <OurPrize />
+
 
             <SectionBlock dotTitle={"Testimonial"} title1={"12기 선배들에게"} title2={"앱앤미란?"}
                 marginTop={"75px"} marginBottom={"55px"} />
             <MentorVoicesCard />
             <Bottom />
-            <FaQ />
+            <div className={styles.styleContainer}>
+                <FaQ />
+            </div>
             <JoinUsNow />
             <Footer />
         </div>
