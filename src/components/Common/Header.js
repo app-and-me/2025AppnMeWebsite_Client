302c88b199d46d293bc3fdc67285b65267e35dfa
hofkj <carrot070410@naver.com>
2025-03-02 17:47:23 +0900

    Design : 수정

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -4,35 +4,57 @@ import styles from "../../styles/Home/Header.module.css";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function Header() {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    return (
-        <header className={styles.header}>
-            <button 
-                className={styles.menuButton} 
-                onClick={() => setMenuOpen(!menuOpen)}
-                aria-label="메뉴 열기"
-            >
-                {menuOpen ? <FaTimes /> : <FaBars />}
-            </button>
+  return (
+    <header className={styles.header}>
+      <button
+        className={styles.menuButton}
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label="메뉴 열기"
+      >
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </button>
 
-            <div className={styles.logo}>
-                <img src="/images/Logo/logo.png" alt="App & Me Logo" />
-            </div>
+      <div className={styles.logo}>
+        <img src="/images/Logo/logo.png" alt="App & Me Logo" />
+      </div>
 
-            <nav className={styles.nav}>
-                <ul className={menuOpen ? styles.active : ""}>
-                    <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
-                    <li><Link to="/AboutUs" onClick={() => setMenuOpen(false)}>About Us</Link></li>
-                    <li><Link to="/Prize" onClick={() => setMenuOpen(false)}>Prize</Link></li>
-                    <li><Link to="/JoinUs" onClick={() => setMenuOpen(false)}>Join Us</Link></li>
-                    <li><Link to="/QnA" onClick={() => setMenuOpen(false)}>Q&A</Link></li>
-                </ul>
-            </nav>
+      <nav className={styles.nav}>
+        <ul className={menuOpen ? styles.active : ""}>
+          <li>
+            <Link to="/" onClick={() => setMenuOpen(false)}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/AboutUs" onClick={() => setMenuOpen(false)}>
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link to="/Prize" onClick={() => setMenuOpen(false)}>
+              Prize
+            </Link>
+          </li>
+          <li>
+            <Link to="/JoinUs" onClick={() => setMenuOpen(false)}>
+              Join Us
+            </Link>
+          </li>
+          <li>
+            <Link to="/QnA" onClick={() => setMenuOpen(false)}>
+              Q&A
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
-            <div className={styles.applyButton}>
-                <button>지원하기</button>
-            </div>
-        </header>
-    );
+      <div className={styles.applyButton}>
+        <Link to="/JoinUs">
+          <button>지원하기</button>
+        </Link>
+      </div>
+    </header>
+  );
 }
