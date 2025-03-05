f48d8a1d375c1bebfcb6e183d2927909cb492898
hofkj <carrot070410@naver.com>
2025-03-05 22:59:49 +0900

    Design : 클래스 네임 변경

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import styles from "../../styles/Home/Footer.module.css"
+import styles from "../../styles/Home/Footer.module.css";
+
 export default function Footer() {
 
     const MoveToTop = () => {
         window.scrollTo({ top: 0, behavior: "smooth" })
     }
+
     const navigate = useNavigate()
+
     const handleClick = () => {
         navigate('/')
         MoveToTop()
@@ -21,21 +24,21 @@ export default function Footer() {
                 </div>
 
                 <div className={styles.pagesSection}>
-                    <div classNam={styles.pages}>Pages</div>
-                    <ul>
-                        <li onClick={MoveToTop}><Link to="/">Home</Link></li>
-                        <li onClick={MoveToTop}><Link to="/AboutUs">About Us</Link></li>
-                        <li onClick={MoveToTop}><Link to="/Prize">Prize</Link></li>
-                        <li onClick={MoveToTop}><Link to="/JoinUs">Join Us</Link></li>
-                        <li onClick={MoveToTop}><Link to="/QnA">Q&A</Link></li>
+                    <div className={styles.pages}>Pages</div>
+                    <ul className={styles.pagesList}> {/* ul에 클래스 추가 */}
+                        <li className={styles.pageItem} onClick={MoveToTop}><Link to="/">Home</Link></li> {/* li에 클래스 추가 */}
+                        <li className={styles.pageItem} onClick={MoveToTop}><Link to="/AboutUs">About Us</Link></li>
+                        <li className={styles.pageItem} onClick={MoveToTop}><Link to="/Prize">Prize</Link></li>
+                        <li className={styles.pageItem} onClick={MoveToTop}><Link to="/JoinUs">Join Us</Link></li>
+                        <li className={styles.pageItem} onClick={MoveToTop}><Link to="/QnA">Q&A</Link></li>
                     </ul>
                 </div>
 
                 <div className={styles.socialSection}>
                     <div className={styles.social}>Social</div>
 
-                    <ul>
-                        <li>
+                    <ul className={styles.socialList}> {/* ul에 클래스 추가 */}
+                        <li className={styles.socialItem}> {/* li에 클래스 추가 */}
                             <a href="https://www.instagram.com/app_and_me?igsh=a25jNHFzMmF6cHJi">
                                 Instagram
                             </a>
