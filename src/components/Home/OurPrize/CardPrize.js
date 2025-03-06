f0f9de987e173f1195fdb63e1f79533fb397a116
juyeon Bae <w2316@e-mirim.hs.kr>
2025-03-06 10:05:10 +0900

    fix : 수상 내역 크기 조정

--- src/components/Home/OurPrize/CardPrize.js
+++ src/components/Home/OurPrize/CardPrize.js
@@ -1,41 +1,18 @@
 import { useEffect, useState } from "react"
 import styles from "../../../styles/Prize/CardPrize.module.css"
-export default function CardPrize(){
-    const [prizeData, setPrizeData] = useState([])
-    useEffect(()=>{
-        const fetchData = async ()=>{
-            try {
-                const response = await fetch("/data/Prize.json");
-                const data = await response.json();  
-                setPrizeData(data);  
-            } catch (error) {
-                console.error("에러 발생", error);
-            }
-        }   
-        fetchData();
-    },[])
-    return(
+import Button from "../../Common/Button"
+export default function CardPrize() {
+    const imgSrc = "/images/Prize/prize"
+
+    return (
         <div className={styles.allContainer}>
-            {prizeData.map((item)=>(
-                <div className={styles.contentContainer}>
-                    <img src={item.src}></img>
-                    <span style={{display:"flex", fontSize:"18px"}}>{item.name}</span>
 
+            <div className={styles.contentContainer}>
+                <img src={`${imgSrc}1.png`}></img>
+                <img src={`${imgSrc}2.png`}></img>
+                <img src={`${imgSrc}2.png`}></img>
+            </div>
 
-                   <div style={{display:"flex", gap:"10px", marginTop:"15px"}}> 
-                    <div className={styles["infoBtn"]}>
-                        <span style={{ color: "white" }}>{item.mainType}</span>
-                        <span style={{ color: "#FFFFFF99" }}> / {item.subType}</span>
-                    </div>
-
-                    <div className={styles["infoBtn"]}>
-                        <span>{item.number}</span>
-                        <span>APP & ME</span>
-                    </div>
-                    </div>
-
-                </div>
-            ))}
-        </div>
+        </div >
     )
 }
