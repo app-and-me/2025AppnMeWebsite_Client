c001e1b0ab834b740793b69c7bdacc80e06a8ca7
juyeon Bae <w2316@e-mirim.hs.kr>
2025-03-08 18:11:27 +0900

    fix : qna page에 banner 추가

--- src/components/Home/MainTitle/Banner.js
+++ src/components/Home/MainTitle/Banner.js
@@ -1,9 +1,9 @@
 import styles from "../../../styles/Home/Banner.module.css"
-export default function Banner(){
-    return(
-        
+export default function Banner({ height }) {
+    return (
+
         <div className={styles.bannerContainer}>
-            <img  src="images/Logo/backGif.gif"></img>
-            </div>
+            <img src="images/Logo/backGif.gif" style={{ height }}></img>
+        </div>
     )
 }
