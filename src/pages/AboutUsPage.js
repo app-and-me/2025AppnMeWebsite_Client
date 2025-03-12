c65c77225ddea7a2e399d645d93ce6fc8b8eedd4
juyeon Bae <w2316@e-mirim.hs.kr>
2025-03-12 22:15:28 +0900

    docs : 링크 수정

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -39,20 +39,20 @@ export default function AboutUsPage() {
     }, []);
 
     const Member12 = [
-        { name: "추다율", github: "dayul" },
-        { name: "윤주영", github: "juyoung07" },
-        { name: "배주연", github: "juyeon-Bae" },
-        { name: "황유진", github: "hofkj" },
-        { name: "하지민", github: "hovynq" },
-        { name: "송유빈", github: "https://github.com/username6" }
+        { name: "추다율", github: "https://github.com/dayul" },
+        { name: "윤주영", github: "https://github.com/juyoung07" },
+        { name: "배주연", github: "https://github.com/juyeon-Bae" },
+        { name: "황유진", github: "https://github.com/hofkj" },
+        { name: "하지민", github: "https://www.instagram.com/hovynq/" },
+        { name: "송유빈", github: "https://www.instagram.com/sonououorn.xo/" }
     ];
     const Member13 = [
-        { name: "지수민", github: "cuzurmyhabit" },
-        { name: "이효은", github: "Hyotaccato" },
-        { name: "유성윤", github: "3x-haust" },
-        { name: "강재호", github: "jaehokang1007" },
-        { name: "이서현" },
-        { name: "송지아" }
+        { name: "지수민", github: "https://github.com/cuzurmyhabit" },
+        { name: "이효은", github: "https://github.com/Hyotaccato" },
+        { name: "유성윤", github: "https://github.com/3x-haust" },
+        { name: "강재호", github: "https://github.com/jaehokang1007" },
+        { name: "이서현", github: "https://www.instagram.com/zush.ol/" },
+        { name: "송지아", github: "https://www.instagram.com/soo_g81/" }
     ];
     return (
         <div>
@@ -96,14 +96,15 @@ export default function AboutUsPage() {
                 <div>
                     <div className={styles.profileContainer}>
                         {Member13.map((item) => (
-                            <a href={`https://github.com/${item.github}`} target="_blank" >
+                            <a href={item.github} target="_blank" >
                                 <img className={styles["profile-img"]} src={`images/Member/13-${item.name}.png`}></img>
                             </a>
                         ))}
                     </div>
                 </div>
-
-                <Bottom />
+                <div className={styles.bottom}>
+                    <Bottom />
+                </div>
                 <div className={styles.faqContainer}>
                     <FaQ />
                 </div>
